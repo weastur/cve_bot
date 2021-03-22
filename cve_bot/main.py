7537ff2b784177c3cbe307349b0a34b56756cc61
Pavel Sapezhko <me@weastur.com>
2021-03-22 15:43:33 +0300

    Simple echo bot

--- cve_bot/main.py
+++ cve_bot/main.py
@@ -1,2 +1,43 @@
-def main():
-    """Not implemented yet."""
+import logging
+import os
+
+from telegram import Update
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text("Hi!")
+
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text("Help!")
+
+
+def echo(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    updater = Updater(os.environ["CVE_BOT_TOKEN"])
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
